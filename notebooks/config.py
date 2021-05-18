96ef407d2b49095e397832bcd5679755ce475841
Steven Yan <examsherpa@gmail.com>
2021-05-18 16:37:17 -0400

    update repository files

--- notebooks/config.py
+++
@@ -1,9 +0,0 @@
-# .gitignore should include reference to config.py
-
-keys = dict(
-    api_key = "Y1abhkdSXsHSgvBdNgi2j4vKC5",
-    api_secret = "ohNinZE3nZrk42N8N9BRN3qoonNxgEleNAZI6sc3ZG3zZTFPBa", 
-    access_token = "1299634175792775168-ELN3eVbmtgoxs0inZEQmfHW05qZ1vJ", 
-    token_secret = "JQxPmuaLzhjpc9yiOdILq0VmSmqDY5NKI4xXfOCD3HYRc",
-    bearer_token = "AAAAAAAAAAAAAAAAAAAAAC%2BENQEAAAAAxb1C9BG2RGm83EJddoJuWHFlgaA%3D2yNbbf6QW9Wwl1wW9P4sF0CjLZgIirGK19VStkFloKNAwD9gXP"
-)
