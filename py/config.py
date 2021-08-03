c602e41d70ed553ed100d98e0793e28ef9e15f2d
Steven Yan <examsherpa@gmail.com>
2021-08-03 01:58:44 -0400

    update files

--- py/config.py
+++
@@ -1,9 +0,0 @@
-# .gitignore should include reference to config.py
-
-keys = dict(
-    api_key = "UzDrdsnrF5cPeEUzpRs1Mq7tR",
-    api_secret = "3oS3Cd7sWnTcAAdsvtQVZPL0JwzzqJRpg9e6CoDxGyp7HG2XwD", 
-    access_token = "1299634175792775168-VACW546UFZ1g0IQZZYecIYRF68Zzxw", 
-    token_secret = "rWMODGcf9u9acqBlCwojHjqUkBpSzvGa30Vd67kcj5tuK",
-    bearer_token = "AAAAAAAAAAAAAAAAAAAAAC%2BENQEAAAAAu3t2bp8Pl5fONEpzta2D0GDIEZU%3D9IopeIBBxZoX2JTiD5aTCzHAzPKCYgnrUJl31JCro6inR8DJ55"
-)
